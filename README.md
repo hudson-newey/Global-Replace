e13658a98f031f0749ff4dc9d7424ec7152103e6
hudson-newey <grath@duck.com>
2022-10-11 00:07:12 +1000

    cleaned up code quality and fixed typo

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# GLBOAL REPLACE
+# GLOBAL REPLACE
 
 A simple script to replace all files in a directory (and all sub directorys) in one motion
+
+---
+
+To Use:  
+`
+$ ruby ./replace.rb [find string] [replacement string]
+`
--- replace.rb
+++ replace.rb
@@ -1,54 +1,50 @@
-class GlobalReplace
+$PROGRAM_NAME = "./replace.rb"
 
-    def __main__(inputString, outputString)
-        @@allFiles = Dir.glob("./**/*").reject { |f| File.directory?(f) }
+def main(inputString, outputString)
+    @allFiles = Dir.glob("./**/*").reject { |f| File.directory?(f) }
+    
+    for i in (0..@allFiles.length) do
+        # skip the replace.rb file (do not write to program file)
         
-        for i in (0..@@allFiles.length) do
-            # skip the replace.rb file (do not write to program file)
-            
-            if (@@allFiles[i] == "./replace.rb")
-                next
-            end
-
-            # replace occurances and store value inside local variable
-            @replacedText = self.readFile(@@allFiles[i]).gsub(inputString, outputString)
-
-            # write and replace original file with replaced/updated text
-            puts "[#{i}] Completed replace of file: #{@@allFiles[i]}:#{self.writeToFile(@@allFiles[i], @replacedText)}"
+        if (@allFiles[i] == $PROGRAM_NAME)
+            next
         end
 
-        # end program
-        exit
+        # replace occurances and store value inside local variable
+        @replacedText = self.readFile(@allFiles[i]).gsub(inputString, outputString)
+
+        # write and replace original file with replaced/updated text
+        puts "[#{i}] Completed replace of file: #{@allFiles[i]}:#{self.writeToFile(@allFiles[i], @replacedText)}"
     end
 
-    def readFile(fileName)
-        # null statement return
-        if (fileName == nil)
-            return ""
-        end
-
-        @filePointer = File.open(fileName)
-        @file_data = @filePointer.read
-
-        # close file to prevent overflow error
-        @filePointer.close
-
-        return @file_data
-    end
-
-    def writeToFile(fileName, value) 
-        # null statement return
-        if (fileName == nil || fileName == "" || value == nil || value == "")
-            return false
-        end
-        
-        File.open(fileName, "w") { |f| f.write "#{value}" }
-        return true
-    end
+    # end program
+    exit
 end
 
-# ENTRY POINT
-@globalReplaceOBJ = GlobalReplace.new
+def readFile(fileName)
+    # null statement return
+    if (fileName == nil)
+        return ""
+    end
+
+    @filePointer = File.open(fileName)
+    @file_data = @filePointer.read
+
+    # close file to prevent overflow error
+    @filePointer.close
+
+    return @file_data
+end
+
+def writeToFile(fileName, value) 
+    # null statement return
+    if (fileName == nil || fileName == "" || value == nil || value == "")
+        return false
+    end
+    
+    File.open(fileName, "w") { |f| f.write "#{value}" }
+    return true
+end
 
 # call main function
-@globalReplaceOBJ.__main__(ARGV[0].to_s, ARGV[1].to_s)
+main(ARGV[0].to_s, ARGV[1].to_s)
